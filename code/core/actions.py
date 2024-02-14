6a5ec191c8c0d2a8af02cd54021fddf89d2aa6f9
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-02-14 17:46:14 +0100

    set mode

--- code/core/actions.py
+++ code/core/actions.py
@@ -114,6 +114,7 @@ def compute_enabled_actions(As, bs, region_vertices, mode = 'fori_loop', batch_s
         enabled_actions = np.full((len(region_vertices), len(As)), fill_value=False)
 
         for (i, j) in tqdm(zip(starts, ends)):
+            vmap_compute_actions_enabled_in_region(As, bs, region_vertices[i:j]
             enabled_actions[i:j] = vmap_compute_actions_enabled_in_region(As, bs, region_vertices[i:j])
 
     else:
