4790109e1df0966187389a25f993b8731007a86b
Thom Badings <tbadings@ip-145-137-154-210.wlan-int.ru.nl>
2024-02-16 10:40:30 +0100

    fix

--- code/benchmarks/Drone.py
+++ code/benchmarks/Drone.py
@@ -51,8 +51,8 @@ class Drone2D(object):
         self.uMin = [-4, -4]
         self.uMax = [4, 4]
 
-        self.partition['boundary'] = np.array([[-7, -3, -7, -3], [7, 3, 7, 3]])
-        self.partition['number_per_dim'] = np.array([7, 4, 7, 4])
+        self.partition['boundary'] = np.array([[-7, -3, -7, -3], [7, 3, 7, 3]]) * 2
+        self.partition['number_per_dim'] = np.array([7, 4, 7, 4]) * 2
 
         self.goal = np.array([
             [[5, -3, 5, -3], [7, 3, 7, 3]]
