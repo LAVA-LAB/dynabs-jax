b7bd9b57c24bd64406d3a6e41db5f7e44bacbedf
Thom Badings <tbadings@ip-145-137-154-210.wlan-int.ru.nl>
2024-02-19 12:48:00 +0100

    change

--- code/core/probabilities.py
+++ code/core/probabilities.py
@@ -72,6 +72,9 @@ def compute_samples_per_state(args, model, partition, target_points, noise_sampl
                                                         batch_size)
 
     if args.debug:
+        import sys
+        import numpy
+        numpy.set_printoptions(threshold=sys.maxsize)
         print(result[0] - result[1])
         assert np.all(result[0] == result[1])
 
