28ca59d56b2d0c6be9728ba072cf1cd2f3bf2445
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-02-20 18:35:59 +0100

    force .gitignore

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
 # Run on server
-  python RunFile.py --prism_dir ~/Documents/SBA/prism-imc/prism/bin/prism
+python RunFile.py --prism_dir ~/Documents/SBA/prism-imc/prism/bin/prism --prism_dir ~/Documents/SBA/prism-imc/prism/bin/prism
