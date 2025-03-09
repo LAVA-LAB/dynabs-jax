6577beef669b4dd91a2a3fa19d5b3ab0463c58ef
Thom Badings <thobad@ip-218-108.eduvpn.ru.nl>
2025-03-09 11:37:48 +0000

    test on server

--- python/RunFile.py
+++ python/RunFile.py
@@ -133,7 +133,7 @@ else:
     #                                            interval_table,
     #                                            round_probabilities=True)
 
-    P_full, P_absorbing = compute_probabilities(model, partition, actions.vertices)
+    # P_full, P_absorbing = compute_probabilities(model, partition, actions.vertices)
 
     P_full2, P_absorbing2 = compute_probabilities_per_dim(model, partition, actions.vertices)
 
